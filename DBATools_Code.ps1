c5fd6b09d6c34f34d38a70cbd6783dbdae450431
Jeff Chapman <jchapman91@gmail.com>
2018-06-24 22:22:50 -0400

    Update DBATools_Code.ps1

--- DBATools_Code.ps1
+++ DBATools_Code.ps1
@@ -4,4 +4,8 @@ start-process https://dbatools.io/snowball
 start-process https://dbatools.io/schwifty 
 start-process https://dbatools.io/functions/
 Get-DbaConfigValue -FullName 'path.dbatoolslogpath' | Invoke-Item 
+import-module dbatools
+Write-Output "dbatools module successfully loaded" 
+function prompt {"PS [$env:username] > "}
+
 
